77c95c4fde49fe0cbd5ef2a557f90dd2742e3e7a
amaged1896 <ahmed.maged1896@gmail.com>
2023-07-09 02:39:35 +0300

    "Main Layout"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import react from 'react';
 import Navbar from './components/Navbar.tsx';
 import Main from './components/Main.tsx';
+import Counter from './components/Counter.tsx';
+import Services from './components/Services.tsx';
+import Rate from './components/Rate.tsx';
+import { Container } from '@mui/material';
+import ContactUs from './components/ContactUs.tsx';
 
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <Container>
+        <Main />
+        <Counter />
+        {/* <Services /> */}
+        <Rate />
+        <ContactUs />
+      </Container>
     </>
   );
 }
