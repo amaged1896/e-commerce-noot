1dade4ec13f9ad5f931cd0864fdd2228c4e13524
amaged1896 <ahmed.maged1896@gmail.com>
2023-07-08 20:43:34 +0300

    "project initialization"

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import Navbar from './components/Navbar.tsx';
+import Main from './components/Main.tsx';
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
